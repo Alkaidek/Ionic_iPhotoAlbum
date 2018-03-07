2e76e53b50c97d246c0b99b265d878d247f70451
MIMEK\Mati <matib16@gmail.com>
2018-03-07 23:01:04 +0100

    my 1st cahnges

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAZVw1fBCArYVvGLLyso7hg4hNGl6wmLD4",
+    authDomain: "ialbum-5113a.firebaseapp.com",
+    databaseURL: "https://ialbum-5113a.firebaseio.com",
+    projectId: "ialbum-5113a",
+    storageBucket: "ialbum-5113a.appspot.com",
+    messagingSenderId: "349252961061"
+  };
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { storage, initializeApp } from 'firebase';
+import { FIREBASE_CONFIG } from "../../app/firebase.config";
+import { Camera, CameraOptions } from "@ionic-native/camera";
+import { ToastController } from "ionic-angular";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private camera: Camera, public navCtrl: NavController, private _tc: ToastController) {
+    initializeApp(FIREBASE_CONFIG);
+  }
+  async takePhoto(sourceType:number){
+    try{
+    //Defining camera options
+      const options: CameraOptions = {
+        quality: 50,
+        targetHeight: 600,
+        targetWidth: 600,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+      }
+      const result =  await this.camera.getPicture(options);
+      const image = `data:image/jpeg;base64,${result}`;
 
+      const pictures = storage().ref('pictures/myPhoto' + (Math.floor(Math.random() * 6) + 1));
+      pictures.putString(image, 'data_url');
+      let optionsToast = {
+        message: "Your photo is taken!",
+        duration: 3000,
+        cssClass: "toast.scss"
+       };
+      
+      this._tc.create(optionsToast).present();
+    }
+    catch (e){
+      console.error(e);
+    }
   }
 
 }
