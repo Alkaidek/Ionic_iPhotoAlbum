2acc64042fcbcf37fa5a78a0e1557212f7641c53
MIMEK\Mati <matib16@gmail.com>
2018-03-08 14:20:54 +0100

    WOW 2 buttons!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,7 +14,7 @@ export class HomePage {
   constructor(private camera: Camera, public navCtrl: NavController, private _tc: ToastController) {
     initializeApp(FIREBASE_CONFIG);
   }
-  async takePhoto(sourceType:number){
+  async takePhotoViaGallery(){
     try{
     //Defining camera options
       const options: CameraOptions = {
@@ -29,11 +29,43 @@ export class HomePage {
       const result =  await this.camera.getPicture(options);
       const image = `data:image/jpeg;base64,${result}`;
 
-      const pictures = storage().ref('pictures/myPhoto' + (Math.floor(Math.random() * 6) + 1));
+      const pictures = storage().ref('gallery/myPhoto' + (Math.floor(Math.random() * 6) + 1));
+
       pictures.putString(image, 'data_url');
       let optionsToast = {
         message: "Your photo is taken!",
-        duration: 3000,
+        duration: 4000,
+        showCloseButton: true,
+        cssClass: "home.scss"
+       };
+      
+      this._tc.create(optionsToast).present();
+    }
+    catch (e){
+      console.error(e);
+    }
+  }
+  async takePhotoViaCamera(){
+    try{
+    //Defining camera options
+      const options: CameraOptions = {
+        quality: 50,
+        targetHeight: 600,
+        targetWidth: 600,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+      }
+      const result =  await this.camera.getPicture(options);
+      const image = `data:image/jpeg;base64,${result}`;
+
+      const pictures = storage().ref('camera/myPhoto' + (Math.floor(Math.random() * 6) + 1));
+
+      pictures.putString(image, 'data_url');
+      let optionsToast = {
+        message: "Your photo is taken!",
+        duration: 4000,
+        showCloseButton: true,
         cssClass: "home.scss"
        };
       
