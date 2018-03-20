1c677f52c2946dec298b2e0fe95471da6bf60567
MIMEK\Mati <matib16@gmail.com>
2018-03-20 21:32:29 +0100

    wow so awesome remastered rate wow so good wow!

--- src/pages/photo-details/photo-details.ts
+++ src/pages/photo-details/photo-details.ts
@@ -35,7 +35,19 @@ export class PhotoDetailsPage {
   allRate = 0;
   rateImg="rateDisable";
   ratePath
-
+  imageAltPage = "https://firebasestorage.googleapis.com/v0/b/ialbum-5113a.appspot.com/o/iAlbum%20storage%2Fheart";
+  imageAltPageCnd = ["0.png?alt=media&token=41ac9bb4-e157-4404-baca-f303a46fff5c",
+  "0.5.png?alt=media&token=94f239e0-2f12-45f2-bec9-faa4f0f8f307",
+  "1.png?alt=media&token=4a7d5fcd-a645-40ff-9be2-4529a2b572e6",
+  "1.5.png?alt=media&token=61c07d0a-f414-4f4c-96ab-df6c8f253c2f",
+  "2.png?alt=media&token=94848298-ae57-498b-9e0a-15daf1be2509",
+  "2.5.png?alt=media&token=5c13fa7d-7fd7-4520-b49d-c6a636e7a8cf",
+    "3.png?alt=media&token=80c1ea77-ab97-4f73-9e89-66d72c075727",
+    "3.5.png?alt=media&token=477aaa98-d482-4bae-9f4e-bcdd262f5faf",
+    "4.png?alt=media&token=0fcf3f24-102f-4790-8477-0a916bba5900",
+    "4.5.png?alt=media&token=88d82064-ea1f-4346-aec8-620fea8b4821",
+    "5.png?alt=media&token=ea28eb3d-6db2-4b79-a1be-4e5a05775d66"]
+  rateValue
 
 
   constructor(private ofAuth:AngularFireAuth, private fdb: AngularFireDatabase,
@@ -54,15 +66,43 @@ export class PhotoDetailsPage {
     this.rateList = _data; 
     if(this.rateList.length>0){
       console.log(this.rateList.length)
+      for(var j=0; this.rateList.length>j; j++){
+        this.allRate = this.allRate + this.rateList[j].rate;
+      }
       for(var i=0; this.rateList.length>i; i++){
+        
         console.log(this.rateList[i]);
-        this.allRate = this.allRate + this.rateList[i].rate;
         console.log("rate:"+this.allRate);
         if(this.rateList[i].user == this.addr){
           this.show="rateDisable";
           this.rateImg = "rate3";
-          console.log(this.allRate/this.rateList.length);
-          this.ratePath = `../../assets/imgs/heart${this.allRate/this.rateList.length}.png`;
+          
+          this.rateValue = this.allRate/this.rateList.length
+          console.log("wartość rejtu"+this.rateValue);
+          //this.ratePath = `../../assets/imgs/heart${this.allRate/this.rateList.length}.png`;
+          if(this.rateValue>0 && this.rateValue<0.5){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[0]}`;
+          }if(this.rateValue>=0.5 && this.rateValue<1){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[1]}`;
+          } if(this.rateValue>=1 && this.rateValue<1.5){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[2]}`;
+          } if(this.rateValue>=1.5 && this.rateValue<2){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[3]}`;
+          } if(this.rateValue>=2 && this.rateValue<2.5){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[4]}`;
+          } if(this.rateValue>=2.5 && this.rateValue<3){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[5]}`;
+          } if(this.rateValue>=3 && this.rateValue<3.5){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[6]}`;
+          } if(this.rateValue>=3.5 && this.rateValue<4){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[7]}`;
+          } if(this.rateValue>=4 && this.rateValue<4.5){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[8]}`;
+          } if(this.rateValue>=4.5 && this.rateValue<5){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[9]}`;
+          } if(this.rateValue>=5){
+            this.ratePath = `${this.imageAltPage}${this.imageAltPageCnd[10]}`;
+          }
         }
       }
     }
